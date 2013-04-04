606aee9306e635a2c1ce4c9f3928c18e4e623f5d
moznion <moznion@gmail.com>
2013-04-05 01:01:57 +0900

    Add function to add github hooks.

---
+++ set_hook.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+use utf8;
+use 5.012000;
+use FindBin;
+
+my $config = do("$FindBin::Bin/config.pl");
+
+my $github_config = $config->{GitHub};
+my $api_url       = 'https://api.github.com/hub';
+my $base_url      = 'https://github.com';
+my $user          = $github_config->{user};
+my $pass          = $github_config->{pass};
+my $callback      = $github_config->{callback_url};
+
+foreach my $repository ( @{ $github_config->{repositories} } ) {
+    foreach my $hook ( @{ $github_config->{hooks} } ) {
+        my $curl = <<CURL;
+curl -u $user:$pass -i $api_url \\
+-F "hub.mode=subscribe" \\
+-F "hub.topic=$base_url/$user/$repository/events/$hook" \\
+-F "hub.callback=$callback"
+CURL
+
+        system($curl);
+    }
+}
---
+++ start.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+carton exec perl set_hook.pl
+carton exec perl app.pl
