13eb307abd88864b329cefb63929104ec2689295
moznion <moznion@gmail.com>
2013-04-06 00:38:20 +0900

    Clean!

--- app.pl
+++ app.pl
@@ -15,46 +15,6 @@ use Twiggy::Server;
 use Text::VisualWidth;
 use Yancha::Bot;
 
-my $config = do("$FindBin::Bin/config.pl");
-my $bot    = Yancha::Bot->new($config);
-$bot->up();
-
-# Setting for host and port.
-GetOptions( \my %option, qw/host=s port=i/, );
-my $server_conf = $config->{Server};
-$option{host} ||= $server_conf->{host};
-$option{port} ||= $server_conf->{port};
-unless ( $option{host} && $option{port} ) {
-    die '! Please specify host and port in config.pl';
-}
-
-my $app = sub {
-    my $req = Plack::Request->new(shift);
-
-    if ( $req->method eq 'POST' and my $payload = $req->param('payload') ) {
-        my $json = decode_json($payload);
-
-        if ( $json->{issue} ) {
-            _response($json, 'issue');
-        }
-        elsif ( $json->{pull_request} ) {
-            _response($json, 'pull_request');
-        }
-        return [ 200, [], [''] ];
-    }
-
-    print "! Received illegal data.\n";
-    return [ 403, [], ['Forbidden'] ];
-};
-
-my $cv = AnyEvent->condvar;
-
-my $server = Twiggy::Server->new(%option);
-$server->register_service($app);
-say "Ready...";
-
-$cv->recv;
-
 sub _construct_message {
     my %contents = @_;
 
@@ -114,4 +74,46 @@ sub _format_type {
 
     return 'UNKNOWN';
 }
+
+# Get configurations.
+my $config = do("$FindBin::Bin/config.pl");
+
+# Setting for host and port.
+GetOptions( \my %option, qw/host=s port=i/, );
+my $server_conf = $config->{Server};
+$option{host} ||= $server_conf->{host};
+$option{port} ||= $server_conf->{port};
+unless ( $option{host} && $option{port} ) {
+    die '! Please specify host and port in config.pl';
+}
+
+my $app = sub {
+    my $req = Plack::Request->new(shift);
+
+    if ( $req->method eq 'POST' and my $payload = $req->param('payload') ) {
+        my $json = decode_json($payload);
+
+        if ( $json->{issue} ) {
+            _response($json, 'issue');
+        }
+        elsif ( $json->{pull_request} ) {
+            _response($json, 'pull_request');
+        }
+        return [ 200, [], [''] ];
+    }
+
+    print "! Received illegal data.\n";
+    return [ 403, [], ['Forbidden'] ];
+};
+
+my $bot    = Yancha::Bot->new($config);
+$bot->up();
+
+my $cv = AnyEvent->condvar;
+
+my $server = Twiggy::Server->new(%option);
+$server->register_service($app);
+say "Ready...";
+
+$cv->recv;
 __END__
