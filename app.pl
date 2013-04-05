7fb0028a5dc30c807459cdb3aa57b3c168363d0b
moznion <moznion@gmail.com>
2013-04-06 00:36:45 +0900

    Integrated some functions.

--- app.pl
+++ app.pl
@@ -28,7 +28,6 @@ unless ( $option{host} && $option{port} ) {
     die '! Please specify host and port in config.pl';
 }
 
-# FIXME a little duplicated!!
 my $app = sub {
     my $req = Plack::Request->new(shift);
 
@@ -36,58 +35,10 @@ my $app = sub {
         my $json = decode_json($payload);
 
         if ( $json->{issue} ) {
-            my $issue = $json->{issue};
-
-            my $repo_name = $json->{repository}->{name};
-            my $number    = $issue->{number};
-            my $title     = $issue->{title};
-            my $state     = $json->{action};
-            my $url       = $issue->{html_url};
-            if ( $json->{comment} ) {
-                $state = "posted";
-                $url   = $json->{comment}->{html_url};
-            }
-            my $body = _omit_trailing($issue->{body});
-
-            my $message = decode_utf8(
-                _construct_message(
-                    type      => 'Issue',
-                    repo_name => $repo_name,
-                    state     => $state,
-                    title     => $title,
-                    number    => $number,
-                    body      => $body,
-                    url       => $url,
-                )
-            );
-
-            print encode_utf8($message) . "\n";
-            $bot->post_yancha_message($message);
+            _response($json, 'issue');
         }
         elsif ( $json->{pull_request} ) {
-            my $pull_request = $json->{pull_request};
-
-            my $repo_name = $json->{repository}->{name};
-            my $number    = $pull_request->{number};
-            my $title     = $pull_request->{title};
-            my $state     = $json->{action};
-            my $url       = $pull_request->{html_url};
-            my $body      = _omit_trailing($pull_request->{body});
-
-            my $message = decode_utf8(
-                _construct_message(
-                    type      => 'Pull-Request',
-                    repo_name => $repo_name,
-                    state     => $state,
-                    title     => $title,
-                    number    => $number,
-                    body      => $body,
-                    url       => $url,
-                )
-            );
-
-            print encode_utf8($message) . "\n";
-            $bot->post_yancha_message($message);
+            _response($json, 'pull_request');
         }
         return [ 200, [], [''] ];
     }
@@ -117,5 +68,50 @@ sub _omit_trailing {
     my ($text, $num) = @_;
 
     return Text::VisualWidth::UTF8::trim(encode_utf8($text), $num);
+}
+
+sub _response {
+    my ($json, $type) = @_;
+
+    my $contents = $json->{$type};
+
+    my $repo_name = $json->{repository}->{name};
+    my $number    = $contents->{number};
+    my $title     = $contents->{title};
+    my $state     = $json->{action};
+    my $url       = $contents->{html_url};
+    if ( $json->{comment} ) {
+        $state = "posted";
+        $url   = $json->{comment}->{html_url};
+    }
+    my $body = _omit_trailing($contents->{body});
+
+    my $message = decode_utf8(
+        _construct_message(
+            type      => _format_type($type),
+            repo_name => $repo_name,
+            state     => $state,
+            title     => $title,
+            number    => $number,
+            body      => $body,
+            url       => $url,
+        )
+    );
+
+    print encode_utf8($message) . "\n";
+    $bot->post_yancha_message($message);
+}
+
+sub _format_type {
+    my $type = shift;
+
+    if ($type eq 'issue') {
+        return 'Issue';
+    }
+    elsif ($type eq 'pull_request') {
+        return 'Pull-Request';
+    }
+
+    return 'UNKNOWN';
 }
 __END__
